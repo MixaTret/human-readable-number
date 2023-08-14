9ae10ea3bc9d48dab5c986419608c406292bbeed
MixaTret <mikhail.tretsiakevich@gmail.com>
2023-08-14 22:24:35 +0100

    init: script of function toReadable  was writting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const secondTen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    const hundred = Math.floor(number / 100);
+    const remainder = number - hundred * 100;
+
+    let result = '';
+    if (hundred > 0) {
+        result += units[hundred] + ' hundred';
+        if (remainder > 0) {
+            result += ' ';
+        }
+    }
+    if (remainder >= 10 && remainder <= 19) {
+        result += secondTen[remainder - 10];
+    } else if (remainder >= 20) {
+        const ten = Math.floor(remainder / 10);
+        const unit = remainder % 10;
+        result += tens[ten] + (unit > 0 ? ` ${units[unit]}` : '');
+    } else if (remainder > 0) {
+        result += units[remainder];
+    }
+    return result.trim();
 }
